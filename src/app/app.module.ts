b81ffe398026abc78d2304564519a3b61f194100
Victor Carvalho <victorcarvalhosp@gmail.com>
2018-09-07 02:16:09 -0300

    Adicionado angular-material ao projeto e adicionado componentes a tela de cadastro de usuário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AngularFireModule} from "@angular/fire";
 import {AngularFirestoreModule} from "@angular/fire/firestore";
 import {AngularFireAuthModule} from "@angular/fire/auth";
 import {AngularFireStorageModule} from "@angular/fire/storage";
 import {environment} from "../environments/environment.credentials";
-import { RegisterComponent } from './pages/register/register.component';
+import {RegisterComponent} from './pages/register/register.component';
 import {LoginComponent} from "./pages/login/login.component";
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import {LandingPageComponent} from './pages/landing-page/landing-page.component';
 import {appRoutes} from "./app.routes";
 import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatInputModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -22,13 +24,16 @@ import {ReactiveFormsModule} from "@angular/forms";
     LandingPageComponent
   ],
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
+    AngularFireAuthModule, // import,s firebase/auth, only needed for auth features,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     AngularFireStorageModule, // imports firebase/storage only needed for storage features
-    RouterModule.forRoot(appRoutes)
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
